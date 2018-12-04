758303474cd262ebde79fab7105dca9e450ddec6
naotoh0811 <ssbmx0811@gmail.com>
2018-12-04 14:57:36 +0900

    cout format

--- vec_sch.cpp
+++ vec_sch.cpp
@@ -7,33 +7,32 @@ void find_model_example(int x_min) {
     std::cout << "find_model_example1\n";
     context c;
     expr_vector T(c);
-    expr x = c.int_const("xxx");
+    expr x = c.int_const("x");
     solver s(c);
 
-    for(int i = 0; i<10; i++){
+    for(int i = 0; i < 10; i++){
         std::stringstream Tname;
         Tname<<"Tname_"<<i;
         T.push_back(c.int_const(Tname.str().c_str()));
     }
 
     s.add(T[1] == 100);
-    s.add(T[2] == 200);
+    s.add(T[2] == T[1]);
     s.add(x == 1);
-    std::cout << s.check() << "\n" << "-------" << "\n";
+    std::cout << s.check() << "\n";
 
-    std::cout << s << "\n" << "-------" << "\n";
+    std::cout << "--solver s--\n" << s << "\n";
 
     model m = s.get_model();
-    std::cout << m << "\n" << "-------" << "\n";
+    std::cout << "--model m--\n" <<  m << "\n";
     // traversing the model
-    for (unsigned i = 0; i < m.size(); i++) {
+    std::cout << "\n--results--\n";
+    for (int i = 0; i < m.size(); i++) {
         func_decl v = m[i];
         // this problem contains only constants
         assert(v.arity() == 0); 
         std::cout << v.name() << " = " << m.get_const_interp(v) << "\n";
     }
-    // we can evaluate expressions in the model.
-    //std::cout << "x + y + 1 = " << m.eval(x + y + 1) << "\n";
 }
 
 
