f0ae7b0e4b20d4c71a51a755082a926e12d0903b
naotoh0811 <ssbmx0811@gmail.com>
2019-06-07 17:19:45 +0900

    [add] 2sw_sch

---
+++ 2sw_sch/2sw_sch.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <fstream>
+#include "z3++.h"
+#include "../func.h"
+
+using namespace z3;
+
+void find_model_example() {
+    std::cout << "find_model_example1\n";
+    context c;
+    expr_vector optime(c);
+    expr_vector cltime(c);
+    expr_vector que(c);
+    expr x = c.int_const("x");
+    solver s(c);
+
+    // request
+    int ocu_time = 3;
+    int cycle_time[] = {20, 30, 40, 60};
+
+    // various variables
+    int num_flow; // the number of all flows
+    num_flow = sizeof(cycle_time) / sizeof(*cycle_time);
+
+    int schedule_cycle; // one cycle time of schedule
+    schedule_cycle = multi_lcm(cycle_time, num_flow);
+
+    int num_win = 0; // the number of all windows
+    int i_win_first[num_flow]; // index of first window of each flow
+    int i_win_last[num_flow]; // index of last window of each flow
+    int each_num_win = 0;
+    for(int i = 0; i < num_flow; i++){
+        i_win_first[i] = num_win;
+        num_win += schedule_cycle / cycle_time[i];
+        i_win_last[i] = num_win - 1;
+    }
+
+    // define vector
+    for(int i = 0; i < num_win; i++){
+        std::stringstream optime_name, cltime_name, que_name;
+        optime_name << "optime_" << i;
+        cltime_name << "cltime_" << i;
+        que_name << "que_" << i;
+        optime.push_back(c.int_const(optime_name.str().c_str()));
+        cltime.push_back(c.int_const(cltime_name.str().c_str()));
+        que.push_back(c.int_const(que_name.str().c_str()));
+    }
+
+    /********** Constraint **********/
+
+    // flow cycle
+    for(int i = 0; i < num_flow; i++){
+        for(int j = i_win_first[i]; j < i_win_last[i] + 1; j++){
+            if(j != i_win_last[i]){
+                s.add(optime[j+1] - optime[j] == cycle_time[i]);
+            }else{
+                s.add( (schedule_cycle - optime[j]) + (optime[i_win_first[i]] - 0) == cycle_time[i] );
+            }
+        }
+    }
+
+    // exclusiveness
+    for(int i = 0; i < num_win; i++){
+        for(int j = 0; j < num_win; j++){
+            if(i != j){
+                s.add(cltime[i] <= optime[j] || cltime[i] >= cltime[j]);
+            }
+        }
+    }
+
+    // occupancy time
+    for(int i = 0; i < num_win; i++){
+        s.add(cltime[i] - optime[i] == ocu_time);
+    }
+
+    // range of schedule cycle
+    for(int i = 0; i < num_win; i++){
+        s.add(optime[i] >= 0);
+        s.add(cltime[i] <= schedule_cycle);
+    }
+
+    /********************************/
+
+    std::cout << "-----sat check-----" << "\n"; 
+    std::cout << s.check() << "\n";
+    std::cout << "-------------------" << "\n"; 
+
+    model m = s.get_model();
+    //std::cout << "-----model m-----" << "\n"; 
+    //std::cout << m << "\n";
+    //std::cout << "-----------------" << "\n"; 
+
+    // make yaml file
+    std::ofstream out("schedule.yml");
+    out << "num_flow : " << num_flow << "\n";
+    out << "schedule_cycle: " << schedule_cycle << "\n";
+    out << "flow:\n";
+    for(int i = 0; i < num_flow; i++){
+        out << "  - name: flow" << i << "\n";
+        out << "    cycle: " << cycle_time[i] << "\n";
+        out << "    time:\n";
+        for(int j = i_win_first[i]; j <  i_win_last[i] + 1; j++){
+            out << "      - [" << m.eval(optime[j]) << "," << m.eval(cltime[j]) << "]\n";
+        }
+    }
+    out.close();
+
+    std::cout << "schedule cycle time = " << schedule_cycle << "\n";
+    for(int i = 0; i < num_flow; i++){
+        std::cout << "#cycle:" << cycle_time[i] << "\n";
+        for(int j = i_win_first[i]; j < i_win_last[i] + 1; j++){
+            std::cout << m.eval(optime[j]) << ",";
+            std::cout << m.eval(cltime[j]) << "\n";
+        }
+        //std::cout << "\n";
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    int arg = 10;
+    if (argc > 1) arg = atoi(argv[1]);
+    try {
+        find_model_example();
+        std::cout << "\n";
+        std::cout << "done\n";
+    }
+    catch (exception & ex) {
+        std::cout << "unexpected error: " << ex << "\n";
+    }
+    return 0;
+}
+
---
+++ 2sw_sch/dataget.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import yaml
+
+def plot(flow_num, open_time, close_time, hight):
+    plt.broken_barh([(open_time, close_time - open_time)], (hight*(flow_num*2+1), hight))
+
+def read_and_plot(filename):
+    # read yaml
+    f = open(filename, "r+")
+    data = yaml.load(f, yaml.SafeLoader)
+
+    # variables set
+    num_flow = data['num_flow']
+    y_size = 100
+    hight = y_size / (num_flow * 2 + 1)
+    schedule_cycle = data['schedule_cycle']
+
+    # plot
+    for num, flow in enumerate(data['flow']):
+        plt.text(-15, hight*(1.5+num*2), flow['name'], size = 16)
+        for i_time in flow['time']:
+            plot(num, i_time[0], i_time[1], hight)
+
+    plt.axis([0, schedule_cycle, 0, y_size])
+    plt.grid(axis="x")
+    plt.xlabel("Time", fontsize=18)
+    plt.tick_params(labelleft=False, left=False, labelsize=18)
+    plt.tight_layout()
+    pp = PdfPages("window.pdf")
+    pp.savefig()
+    pp.close()
+    plt.clf()
+
+if __name__ == '__main__':
+    read_and_plot("schedule.yml")
