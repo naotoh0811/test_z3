46e54ccc7b60a8ee89f16b4a11d66a0807282fc1
naotoh0811 <hagino@comp.sd.keio.ac.jp>
2019-11-04 15:50:27 +0900

    [add] flow data with path

--- network/dijkstra/gen_flowData.py
+++ network/dijkstra/gen_flowData.py
@@ -1,3 +1,45 @@
 import subprocess
+import yaml
 
-subprocess.run(["ls", "-lt"])
+def read_yaml(filename):
+    f = open(filename, "r+")
+    data = yaml.load(f, yaml.SafeLoader)
+    return data
+
+def gen_csv_from_data(data, csv_filename):
+    write_first_line(csv_filename)
+    for flow in data:
+        name = flow["name"]
+        src = flow['src']
+        dst = flow["dst"]
+        cycle = flow["cycle"]
+        size = flow["size"]
+
+        output = subprocess.run(["./main.o", str(src), str(dst)], stdout=subprocess.PIPE)
+        # convert to str
+        output = output.stdout.decode("utf8")
+        # delete \n
+        output = output.rstrip('\n')
+        nodes_reverse = output.split(",")[1]
+        node_list_reverse = nodes_reverse.split()
+        node_list = list(reversed(node_list_reverse))
+
+        file_output = name + "," + str(cycle) + ","
+        for node in node_list:
+            file_output += node + " "
+        file_output = file_output.rstrip(' ')
+        file_output += "," + str(size) + "\n"
+
+        with open(csv_filename, 'a') as f:
+            f.write(file_output)
+
+def write_first_line(csv_filename):
+    with open(csv_filename, 'a') as f:
+        f.write("name,cycle,node_list,ocu_time\n")
+
+if __name__ == "__main__":
+    data = read_yaml('../flow/flow.yml')
+    csv_filename = 'flow_with_path.csv'
+    with open(csv_filename, 'w'):
+        pass
+    gen_csv_from_data(data, csv_filename)
